e89d293057307a975cb0da338f7f04191cd4db6f
zhirayr07@gmail.com <zhirayr07@gmail.com>
2023-12-11 13:01:23 +0400

    Add project files.

---
+++ _Stream_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace _Stream_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter("test.txt", true, Encoding.UTF8))
+            {
+                sw.WriteLine("Hello ");
+                sw.WriteLine("Hello World");
+                sw.WriteLine("лриромр");
+            }
+
+            using (var sr = new StreamReader("test.txt", Encoding.UTF8))
+            {
+                while(!sr.EndOfStream)
+                {
+                    Console.WriteLine(sr.ReadLine() + " End Line");
+                }
+                var text = sr.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
